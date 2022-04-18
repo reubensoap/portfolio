7f882759c47ec670bc461549de6d9d85c967c802
reuben <reubencode117@gmail.com>
2022-04-17 23:31:16 -0500

    production ready

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,49 @@ const path = require('path');
 
 console.log(path.join(__dirname, 'public'));
 
-module.exports = {
-    entry: './src/App.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+module.exports = (env) => {
+
+    const isProduction = env === 'production';
+
+    return {
+        
+        entry: './src/App.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        },
-        {
-            test: /\.(jpe?p|gif|png|svg)$/i,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?p|gif|png|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000
+                        }
                     }
-                }
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback:true
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback:true
+        }   
     }
-};
+}
+
 
