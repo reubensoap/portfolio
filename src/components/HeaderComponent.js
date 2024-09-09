fbbdaa99b97a60d92c9bfc43672dd4179ebc3677
Reuben Soap <reubencode117@gmail.com>
2024-09-09 15:37:00 -0500

    updated

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,26 +8,19 @@ class Header extends React.Component {
 
     render() {
         return(
-            <div className="header-wrapper spacer">
+            <div className="spacer">
                 <div className="main-text">
-                    <h1>Reuben</h1>
-                    <h1>Pena</h1>
+                    <img src={require('../images/reuben_pena_logo.png')} className="mainImage" />
                 </div>
-                <div className="flex-it sub-header-wrapper black-pill">
-                    <div className="header-details body-nav">
-                        <p>reubencode117@gmail.com</p>
-                        <h3>/Developer</h3>
+                <div className="sub-header-wrapper black-pill">
+                    <div className="flex-it body-nav">
+                        <a target="_blank" href="https://github.com/reubensoap"><p>Github</p></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/reuben-pena/"><p>LinkedIn</p></a>
+                        <NavLink to="privacy"><p>Privacy Policy</p></NavLink>
                     </div>
-                    <div>
-                        <h3>I'm a software engineer specializing in building
-                            exceptional digital experiences.
-                        </h3>
-                        <div className="flex-it body-nav">
-                            <a target="_blank" href="https://github.com/reubensoap"><p>Github</p></a>
-                            <a target="_blank" href="https://www.linkedin.com/in/reuben-pena/"><p>LinkedIn</p></a>
-                            <NavLink to="/privacy"><p>Privacy Policy</p></NavLink>
-                        </div>
-                    </div>
+                    <h3>I'm a software engineer specializing in building
+                        exceptional digital experiences.
+                    </h3>     
                 </div>
             </div>
         );      
